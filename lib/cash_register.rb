7d6efa0a0efe326742025bd6bcef5daefb8e9879
Jack Atherton <jackrrye@gmail.com>
2019-06-20 21:20:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,9 +19,9 @@ class CashRegister
       discount_variable = 100 - @discount
       discount_variable = discount_variable.to_f / 100
       @total = @total * discount_variable
-      puts ""
+      puts "After the discount, the total comes out to $#{@total}."
     else 
-      puts ""
+      puts "There is no discount to apply."
     end 
   end 
     
